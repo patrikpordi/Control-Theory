fcca15da3d47287a5b5ad952ce238e797738ad75
patrikpordi <119634185+patrikpordi@users.noreply.github.com>
2023-02-14 11:33:19 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+To run the simulations go to the files and run the Controller_for_both.m file to initialize the workspace in Matlab.
+The corresponding Simulink models can be found in PID and SMC, open them and run them, and the required figures can be opened as well.
+Since the PID and PD simulations did not provide significant differences the integrator gains are set to zero,
+you can edit them in the corresponding PID blocks.
---
+++ files/Controller_for_both.m
@@ -0,0 +1,34 @@
+I_xx=2.547*10^-3;
+I_yy=3.613*10^-3;
+I_zz=1.074*10^-3;
+l_b=0.083;
+l_c=0.04;
+l_f=0.093;
+l_l=0.185;
+l_t=0.2;
+l_y=0.4;
+b=4.95*10^-5;
+d=7.5*10^-7;
+m=2.03;
+
+g=9.8; % it should be positive
+X_ref=1;
+Y_ref=-1;
+Z_ref=2;
+Psi_ref=pi/6;
+
+
+%%%% SMC
+lambda_x=1;
+lambda_y=1;
+lambda_z=1;
+lambda_phi=6;
+lambda_theta=6;
+lambda_psi=6;
+
+K_k_x=1;
+K_k_y=1;
+K_k_z=0.1*20;
+K_k_phi=1*20;
+K_k_theta=1*20;
+K_k_psi=0.1*10;
